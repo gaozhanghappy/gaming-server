58cb10226b95e1c705e7b78ffd7f26b869cd9f69
gaozhanghappy <gaozhanghappy@126.com>
2019-06-16 10:53:15 +0800

    gaming-server-commons2 222

---
+++ src/main/java/git/gaozhanghappy/gaming/server/commons/GamingServerCommonsApplication.java
@@ -0,0 +1,13 @@
+package git.gaozhanghappy.gaming.server.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingServerCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingServerCommonsApplication.class, args);
+	}
+
+}
